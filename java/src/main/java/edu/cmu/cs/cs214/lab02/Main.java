fc0bb8367929b5f359e2d8dfead48b0923706dbd
miaol <miaol@miaoldeMacBook-Pro.local>
2025-09-05 13:18:18 -0400

    finish capsulation in java version

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
+        Shape rectangle = new Rectangle(2, 3);
+        
         Renderer renderer = new Renderer(rectangle);
+        renderer.draw();    
 
-        renderer.draw();
+        Shape circle = new Circle(5);
+        Renderer rendererCircle = new Renderer(circle);
+        rendererCircle.draw();
+
+        Shape square = new Square(4);
+        Renderer rendererSquare = new Renderer(square);
+        rendererSquare.draw();
     }
 }
